d78a2b0d132c4c9eb14b281ad94941f1de47bb47
anuja12mishra <25anujamishra12@gmail.com>
2025-04-14 21:53:51 +0530

    -

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route,Navigate} from "react-router-dom";
-import Register from "./pages/Register";
+import Register from "./pages/RegisterPage";
 import Login from "./pages/Login";
 import Dashboard from './pages/Dashboard';
 import EmailVerifier from "./pages/EmailVerifier";
